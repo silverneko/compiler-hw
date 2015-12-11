fda0775717e514027fbfe0b08c681caec195fa77
Chiang, Yi-Yo@home <b03902082@ntu.edu.tw>
2015-12-11 16:43:08 +0800

    function call check: ignore first dimension

---
+++ hw4/pattern_hw4/mytest.c
@@ -0,0 +1,19 @@
+int f(int a){}
+
+int g(int a, float b){}
+
+int h(int a[100][1000], float b[][222]){}
+
+int main(){
+  int ivar;
+  int iivar = 10, iarr[10][100][1000];
+  float fvar;
+  float ffvar, iivar, farr[2][22][222];
+  ivar = f(ivar);
+  fvar = g(ivar, fvar);
+  ffvar = f(fvar, fvar);
+  iivar = h(iarr, farr);
+  h(iarr[10], farr[1]);
+  iivar = iarr[1.0];
+  return 0;
+}
--- hw4/semanticAnalysis.c
+++ hw4/semanticAnalysis.c
@@ -497,26 +497,36 @@ void checkWriteFunction(AST_NODE* functionCallNode)
 TypeDescriptor * exprType(AST_NODE * node){
   // TODO : return the kind of the expression, scalar or array
   TypeDescriptor * typedesc = malloc(sizeof(TypeDescriptor));
-  if(node->semantic_value.identifierSemanticValue.kind == ARRAY_ID){
-    AST_NODE * dims = node->child;
-    SymbolTableEntry * arr = retrieveSymbol(idName(node));
-    if(arr == NULL){
-      printErrorMsg(node, SYMBOL_UNDECLARED);
-      return NULL;
+  typedesc->kind = SCALAR_TYPE_DESCRIPTOR;
+  switch(node->semantic_value.identifierSemanticValue.kind){
+    case ARRAY_ID:
+    case NORMAL_ID:
+    {
+      SymbolTableEntry * arr = retrieveSymbol(idName(node));
+      if(arr == NULL){
+        printErrorMsg(node, SYMBOL_UNDECLARED);
+        return NULL;
+      }
+      if(arr->symbolAttribute->typeDescriptor->kind == SCALAR_TYPE_DESCRIPTOR){
+        break;
+      }
+      // array type
+      typedesc->kind = ARRAY_TYPE_DESCRIPTOR;
+      AST_NODE * dims = node->child;
+      int dimsn = 0;
+      while(dims != NULL){
+        ++dimsn;
+        dims = dims->rightSibling;
+      }
+      typedesc->arrayProperties.dimension = arr->symbolAttribute->typeDescriptor->arrayProperties.dimension - dimsn;
+      for(int i = 0; i < typedesc->arrayProperties.dimension; ++i){
+        typedesc->arrayProperties.sizeInEachDimension[i] = 
+          arr->symbolAttribute->typeDescriptor->arrayProperties.sizeInEachDimension[dimsn + i];
+      }
+      break;
     }
-    int dimsn = 0;
-    while(dims != NULL){
-      ++dimsn;
-      dims = dims->rightSibling;
-    }
-    typedesc->kind = ARRAY_TYPE_DESCRIPTOR;
-    typedesc->arrayProperties.dimension = arr->symbolAttribute->typeDescriptor->arrayProperties.dimension - dimsn;
-    for(int i = 0; i < typedesc->arrayProperties.dimension; ++i){
-      typedesc->arrayProperties.sizeInEachDimension[i] = 
-        arr->symbolAttribute->typeDescriptor->arrayProperties.sizeInEachDimension[dimsn + i];
-    }
-  }else{
-    typedesc->kind = SCALAR_TYPE_DESCRIPTOR;
+    default:
+      break;
   }
   return typedesc;
 }
@@ -577,6 +587,10 @@ void checkParameterPassing(Parameter * param, AST_NODE * arg){
           printErrorMsg(arg, INCOMPATIBLE_ARRAY_DIMENSION);
       }else{
         for(int i = 0; i < typeDesc->arrayProperties.dimension; ++i){
+          if(i == 0 && param->type->arrayProperties.sizeInEachDimension[0] == -1){
+            // ignore first dimensino
+            continue;
+          }
           if(typeDesc->arrayProperties.sizeInEachDimension[i] != param->type->arrayProperties.sizeInEachDimension[i]){
             printErrorMsg(arg, INCOMPATIBLE_ARRAY_DIMENSION);
             break;
@@ -662,7 +676,7 @@ void processBlockNode(AST_NODE* blockNode)
 
 void processStmtNode(AST_NODE* stmtNode)
 {
-    printf("%d\n", stmtNode->linenumber);
+    // printf("%d\n", stmtNode->linenumber);
 
     if(stmtNode->nodeType == BLOCK_NODE) {
         processBlockNode(stmtNode);
@@ -709,7 +723,7 @@ int processDeclDimList(AST_NODE* id_node, DATA_TYPE datatype, TypeDescriptor* de
 
     dim = 0;
     AST_ITER_CHILD(id_node, array_node) {
-        if(array_node->nodeType = NUL_NODE) {
+        if(array_node->nodeType == NUL_NODE) {
             if(ignoreFirstDimSize) {
                 if(desc != NULL) {
                     desc->arrayProperties.sizeInEachDimension[dim] = -1;
