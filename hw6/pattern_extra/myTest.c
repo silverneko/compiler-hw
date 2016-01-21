406a7a6fab9a37341d94312fb4d465adcca579d9
Chiang, Yi-Yo@home <b03902082@ntu.edu.tw>
2016-01-21 16:56:42 +0800

    short circuit test case

---
+++ hw6/pattern_extra/myTest.c
@@ -0,0 +1,34 @@
+int zero(){ write("0\n"); return 0;}
+int one(){ write("1\n"); return 1;}
+int two(){ write("2\n"); return 2;}
+int three(){ write("3\n"); return 3;}
+
+int MAIN(){
+  if(one() || zero()){
+    write("correct: 1\n");
+  }else{
+    write("wrongn\n");
+  }
+  if(zero() || one()){
+    write("correct: 0 1\n");
+  }else{
+    write("wrongn\n");
+  }
+
+  if(one() && zero()){
+    write("wrongn\n");
+  }else{
+    write("correct: 1 0\n");
+  }
+  if(zero() && one()){
+    write("wrongn\n");
+  }else{
+    write("correct: 0\n");
+  }
+  if(one() && two()){
+    write("correct: 1 2\n");
+  }else{
+    write("wrongn\n");
+  }
+
+}
